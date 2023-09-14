8d424b65fb254f41a48178d7ca481fa3cbaff6fd
anjwsm <anjalikushwahawsm@gmail.com>
2023-09-14 23:27:06 +0530

    Adding sound effects

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,12 +1,11 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length ;
 
- for (var i = 0 ; i < document.querySelectorAll(".drum").length ; i++ ){ 
-
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-
-    function handleClick() {
-        alert("I got Clicked");
-    }
+ for (var i = 0 ; i < numberOfDrumButtons ; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click" , function(){
+        var audio = new Audio("sounds/ton-1.mp3");
+        audio.play();
+    });
+    
  }
 
 
